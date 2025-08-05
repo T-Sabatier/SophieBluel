e7172d5d754a819d31520049f48132221e592746
T-Sabatier <t.sabatier04@laposte.net>
2025-08-05 10:53:09 +0200

    feat(script): ajouter script.js pour afficher dynamiquement les images

---
+++ FrontEnd/script.js
@@ -0,0 +1,31 @@
+// script.js
+
+// On cible la div qui contient les projets
+const gallery = document.querySelector('.gallery');
+
+fetch('http://localhost:5678/api/works')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Erreur lors de la récupération des projets');
+    }
+    return response.json();
+  })
+  .then(works => {
+    gallery.innerHTML = ''; // Vide la galerie avant insertion
+
+    works.forEach(work => {
+      const figure = document.createElement('figure');
+
+      const img = document.createElement('img');
+      img.src = work.imageUrl;
+      img.alt = work.title;
+
+      const caption = document.createElement('figcaption');
+      caption.innerText = work.title;
+
+      figure.appendChild(img);
+      figure.appendChild(caption);
+      gallery.appendChild(figure);
+    });
+  })
+  

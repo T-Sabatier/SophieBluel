d3b65ddefc1888df57808de7a9a52486f13b568a
T-Sabatier <t.sabatier04@laposte.net>
2025-08-06 12:02:54 +0200

    feat(filters): ajouter le fonctionnement des filtres et le style CSS associé

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,19 +1,8 @@
-// script.js
-
 // On cible la div qui contient les projets
 const gallery = document.querySelector('.gallery');
-
-async function loadGallery() {
-  try {
-    const response = await fetch('http://localhost:5678/api/works');
-
-    if (!response.ok) {
-      throw new Error('Erreur lors de la récupération des projets');
-    }
-
-    const works = await response.json();
-    gallery.innerHTML = '';
-
+ function afficherGallerie(works){
+      gallery.innerText="";
+      
     works.forEach(work => {
       const figure = document.createElement('figure');
 
@@ -29,17 +18,57 @@ async function loadGallery() {
       gallery.appendChild(figure);
     });
 
-  } catch (error) {
+  }
+async function loadGallery() {
+  try {
+    const response = await fetch('http://localhost:5678/api/works');
+
+    if (!response.ok) {
+      throw new Error('Erreur lors de la récupération des projets');
+    }
+
+    const works = await response.json();
+    gallery.innerHTML = '';
+
+    // Gestion des filtres
+    //Recuperation des categories
+    const categorie= works.map(article=> article.category.name)
+    console.log(categorie)
+
+    //Suppression des doublons
+    const categorieUnique= [... new Set (categorie)];
+    console.log(categorieUnique);
+
+    //Cible le conteneur html (filtres)
+    const filtres=document.querySelector(".filtres")
+    const btnAll=document.createElement('button')
+    btnAll.textContent="Tous";
+    filtres.appendChild(btnAll)
+    btnAll.addEventListener("click",()=>{
+      afficherGallerie(works);
+    })
+
+    //Creation des boutons(filtres)
+    categorieUnique.forEach (category=>{
+    const bouton = document.createElement('button');
+    bouton.textContent= category;
+    filtres.appendChild(bouton);
+    bouton.addEventListener("click",()=>{
+    const projetsFiltres = works.filter(work => work.category.name === category);
+    afficherGallerie(projetsFiltres)
+      
+    })
+    })
+    afficherGallerie(works)
+  
+  } 
+  
+  catch (error) {
     console.error(error);
   }
 }
+  
+
 
 // Appel de la fonction au chargement
 loadGallery();
-
-
-  const categorie= projets.map(article=> article.category.name)
-  
-  
-  
-  

38246249b294e9b764cb78067b00c776ac031ced
T-Sabatier <t.sabatier04@laposte.net>
2025-12-15 12:11:35 +0100

    Modification compte admin et correction URL

--- Backend/app.js
+++ Backend/app.js
@@ -19,7 +19,37 @@ const db = require("./models");
 const userRoutes = require('./routes/user.routes');
 const categoriesRoutes = require('./routes/categories.routes');
 const worksRoutes = require('./routes/works.routes');
-db.sequelize.sync().then(()=> console.log('db is ready'));
+const bcrypt = require("bcrypt");
+
+db.sequelize.sync().then(async () => {
+  console.log("db is ready");
+
+  const email = "admin@test.com";
+  const password = "admin1234";
+
+  // selon ton export Sequelize : users ou user
+  const User = db.users || db.user;
+
+  const exists = await User.findOne({ where: { email } });
+  if (!exists) {
+    const hash = await bcrypt.hash(password, 10);
+    await User.create({ email, password: hash });
+    console.log("Admin créé :", email, password);
+  }
+});
+const works = await db.works.findAll();
+for (const w of works) {
+  if (w.imageUrl?.includes("http://localhost:5678")) {
+    w.imageUrl = w.imageUrl.replace(
+      "http://localhost:5678",
+      "https://sophiebluel-api.onrender.com"
+    );
+    await w.save();
+  }
+}
+console.log("imageUrl corrigées");
+
+
 app.use('/api/users', userRoutes);
 app.use('/api/categories', categoriesRoutes);
 app.use('/api/works', worksRoutes);

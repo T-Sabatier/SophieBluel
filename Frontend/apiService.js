683cc3612d4b3e5ececc2fad453b5b0cab19a848
T-Sabatier <t.sabatier04@laposte.net>
2025-12-15 12:04:50 +0100

    Modification du swagger/Url

--- Frontend/apiService.js
+++ Frontend/apiService.js
@@ -1,21 +1,23 @@
+const API_URL = "https://sophiebluel-api.onrender.com";
+
 // Fonction pour se connecter
 async function loginUser(email, password) {
-    const response = await fetch('http://localhost:5678/api/users/login', {
+    const response = await fetch(`${API_URL}/api/users/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password })
     });
-    
+
     if (!response.ok) {
         throw new Error("Erreur d'authentification");
     }
-    
+
     return response.json();
 }
 
 // Fonction pour récupérer la galerie
 async function getWorks() {
-    const response = await fetch('http://localhost:5678/api/works');
+    const response = await fetch(`${API_URL}/api/works`);
 
     if (!response.ok) {
         throw new Error('Erreur lors de la récupération des projets');
@@ -28,15 +30,15 @@ async function getWorks() {
 async function deleteWorks(workId) {
     const token = sessionStorage.getItem("token");
 
-    const response = await fetch(`http://localhost:5678/api/works/${workId}`, {
+    const response = await fetch(`${API_URL}/api/works/${workId}`, {
         method: "DELETE",
         headers: { "Authorization": `Bearer ${token}` }
     });
-    
+
     if (!response.ok) {
         throw new Error("Erreur lors de la suppression");
     }
-    
+
     return true;
 }
 
@@ -49,26 +51,26 @@ async function addWork(title, imageFile, categoryId) {
     infoProjet.append("image", imageFile);
     infoProjet.append("category", categoryId);
 
-    const response = await fetch('https://sophiebluel-api.onrender.com', {
+    const response = await fetch(`${API_URL}/api/works`, {
         method: "POST",
         headers: { "Authorization": `Bearer ${token}` },
         body: infoProjet
     });
-    
+
     if (!response.ok) {
         throw new Error("Erreur lors de l'ajout");
     }
-    
+
     return response.json();
 }
 
 // Récupérer les catégories
 async function getCategory() {
-    const response = await fetch('http://localhost:5678/api/categories');
+    const response = await fetch(`${API_URL}/api/categories`);
 
     if (!response.ok) {
         throw new Error("Erreur lors de la récupération");
     }
-    
+
     return response.json();
 }
